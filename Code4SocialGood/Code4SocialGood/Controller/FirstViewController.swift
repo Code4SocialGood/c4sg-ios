96117de2813259eb7fb1f7a130cda6d082d64a33
Brian Andreasen <brianandreasen1983@users.noreply.github.com>
2018-01-03 11:17:06 -0600

    Project Setup
    This commit is the initial project setup which includes the basic
    project structure and 2 cocoa pods by default.
    
    The 2 cocoa pods are SwiftyJSON and AlamoFire
    
    SwiftyJSON is a JSON parser for swift and Alamofire allows quick and
    elegant HTTP calls for networking.

---
+++ Code4SocialGood/Code4SocialGood/Controller/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Code4SocialGood
+//
+//  Created by iOS Development on 1/3/18.
+//  Copyright © 2018 Code 4 Social Good. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
