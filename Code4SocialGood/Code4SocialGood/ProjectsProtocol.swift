352f75cac13d442d147de91443ec1f3316056a2b
Brian Andreasen <brianandreasen1983@users.noreply.github.com>
2018-01-09 11:49:30 -0600

    wrote three protocols to require implementation of the methods to get the data from the REST API and create loose coupling.

---
+++ Code4SocialGood/Code4SocialGood/ProjectsProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  ProjectsProtocol.swift
+//  Code4SocialGood
+//
+//  Created by iOS Development on 1/9/18.
+//  Copyright © 2018 Code 4 Social Good. All rights reserved.
+//
+
+import Foundation
